03641ae86a8289673d2576905cbd8b64578a3c29
DELL <anhnpqse140645@gmail.com>
2021-08-03 16:09:09 +0700

    Project was intialzied

---
+++ src/main/java/com/internship/InternshipProjectV3/InternshipProjectV3Application.java
@@ -0,0 +1,13 @@
+package com.internship.InternshipProjectV3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipProjectV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipProjectV3Application.class, args);
+	}
+
+}
