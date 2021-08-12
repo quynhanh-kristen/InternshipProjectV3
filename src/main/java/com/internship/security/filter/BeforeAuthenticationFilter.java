31c9eae213b990087c5db6d50a3d027dcb685fdc
ageha-chou <agehachou2602@gmail.com>
2021-08-12 20:28:30 +0700

    add login screen backend validation

---
+++ src/main/java/com/internship/security/filter/BeforeAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package com.internship.security.filter;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BeforeAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final String USERNAME_ERROR_MSG = "Hãy nhập username";
+    private final String PASSWORD_ERROR_MSG = "Hãy nhập password";
+
+    public BeforeAuthenticationFilter() {
+        super.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/login", "POST"));
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(username == null) {
+            username = "";
+        }
+        if(password == null) {
+            password = "";
+        }
+
+        boolean isAnyEmptyField = false;
+
+
+        //check if username is empty
+        if(username.trim().isEmpty()) {
+            request.setAttribute("usernameErr", USERNAME_ERROR_MSG);
+            isAnyEmptyField = true;
+        }
+
+        //keep username
+        request.setAttribute("username", username);
+
+        //check if password is empty
+        if(password.trim().isEmpty()) {
+            request.setAttribute("passwordErr", PASSWORD_ERROR_MSG);
+            isAnyEmptyField = true;
+        }
+
+        if(isAnyEmptyField) {
+            UrlPathHelper helper = new UrlPathHelper();
+            String contextPath = helper.getContextPath(request);
+            try{
+                RequestDispatcher rd = request.getRequestDispatcher(contextPath + "/login");
+                rd.forward(request, response);
+                return null;
+            }catch (IOException | ServletException ex) {
+                ex.printStackTrace();
+            }
+        }
+        try {
+            return super.attemptAuthentication(request, response);
+        } catch (BadCredentialsException badCredentialsException) {
+            request.setAttribute("errorMsg", "Mật khẩu không đúng");
+        } catch(UsernameNotFoundException usernameNotFoundException) {
+            request.setAttribute("errorMsg", "Tên đăng nhập không tồn tại");
+        }
+
+        return super.attemptAuthentication(request, response);
+    }
+}
---
+++ src/main/java/com/internship/utils/AuthenticationCheckingUtils.java
@@ -0,0 +1,16 @@
+package com.internship.utils;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuthenticationCheckingUtils {
+    public static boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.
+                isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
+    }
+}
